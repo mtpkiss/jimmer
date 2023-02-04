181b75ee82650cc76f361c68c39c2a1048383534
babyfish-ct <babyfish.ct@gmail.com>
2023-02-04 17:04:01 +0800

    Let release publish jimmer-dto-compiler

--- project/jimmer-dto-compiler/build.gradle.kts
+++ project/jimmer-dto-compiler/build.gradle.kts
@@ -20,3 +20,57 @@ dependencies {
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
+
+// Publish to maven-----------------------------------------------------
+val NEXUS_USERNAME: String by project
+val NEXUS_PASSWORD: String by project
+
+publishing {
+    repositories {
+        maven {
+            credentials {
+                username = NEXUS_USERNAME
+                password = NEXUS_PASSWORD
+            }
+            name = "MavenCentral"
+            url = if (project.version.toString().endsWith("-SNAPSHOT")) {
+                uri("https://oss.sonatype.org/content/repositories/snapshots")
+            } else {
+                uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            }
+        }
+    }
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            artifactId = "jimmer-dto-compiler"
+            from(components["java"])
+            pom {
+                name.set("jimmer")
+                description.set("immer for java")
+                url.set("https://github.com/babyfish-ct/jimmer")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://github.com/babyfish-ct/jimmer/blob/main/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("babyfish-ct")
+                        name.set("陈涛")
+                        email.set("babyfish.ct@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/babyfish-ct/jimmer.git")
+                    developerConnection.set("scm:git:ssh://github.com/babyfish-ct/jimmer.git")
+                    url.set("https://github.com//babyfish-ct/jimmer")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
